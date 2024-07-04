0711abdb911be3d7b0ec2efc44f9dab584c5e31f
rmorillo <rmorillo@arkusnexus.com>
2024-07-04 16:15:50 -0400

    primer commitj

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { DBZService } from '../services/dbz.service';
+import { Character } from '../interfaces/character.interface';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent {
+
+  constructor( private dbzService: DBZService) {}
+
+  get characters(): Character[] {
+    return [...this.dbzService.characters];
+  }
+
+  onDeleteCharacter(id: string): void {
+    this.dbzService.DeleteCharacterById(id);
+  }
+
+  onNewCharacter(character: Character) {
+    this.dbzService.addCharacter(character);
+  }
+
+}
---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { HeroComponent } from "./Components/hero/hero.component";
+import { ListComponent } from "./Components/list/list.component";
+
+
+@NgModule({
+  declarations: [
+    HeroComponent,
+    ListComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeroComponent,
+    ListComponent
+  ]
+})
+export class HeroesModule {
+
+}
